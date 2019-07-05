68f81694d1b1040281dbe453688e355a6ba6e646
Robby Renz <nxgrenz@gmail.com>
2019-07-05 01:01:42 -0500

    app 1 complete

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,13 @@
+# a simple Hello World program in Python
+
+print() # inserts a blank line
+print('-------------------------------------')
+print('          Hello World App')
+print('-------------------------------------')
+print() # inserts a blank line
+
+user_name = input("What is your name? ") # I intentionally left a space at the end of the sentence so that the cursor will end there
+greeting = f'Nice to meet you, {user_name}!'
+
+print(greeting)
+print()
